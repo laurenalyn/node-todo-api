ccb6a79097f65884709a1b92b64f20d0f3a4720d
laurenalyn <laurenalynwilkerson@gmail.com>
2018-11-08 21:22:02 -0500

    findOneAndDelete

--- playground/mongodb-delete.js
+++ playground/mongodb-delete.js
@@ -10,16 +10,22 @@ MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, client) => {
     const db = client.db('TodoApp');
 
     // deleteMany
-    db.collection('Todos').deleteMany({text: 'eat lunch'})
+    // db.collection('Todos').deleteMany({text: 'eat lunch'})
+    //     .then((result) => {
+    //         console.log(result);
+    //     });
+
+    // deleteOne
+    // db.collection('Todos').deleteOne({text: 'eat lunch'})
+    //     .then((result) =>{
+    //         console.log(result);
+    //     });
+
+    // findOneAndDelete
+    db.collection('Todos').findOneAndDelete({completed: false})
         .then((result) => {
             console.log(result);
         });
-
-    // deleteOne
-    
-
-    // findOneAndDelete
-
 
     // client.close();
 });
