bf9bf8ff148198e3e046ec95fff5b2a95027dec5
laurenalyn <laurenalynwilkerson@gmail.com>
2018-11-08 20:07:52 -0500

    uses destructuring to access mongodb properties

--- playground/mongodb-connect.js
+++ playground/mongodb-connect.js
@@ -1,4 +1,5 @@
-const MongoClient = require('mongodb').MongoClient;
+// const MongoClient = require('mongodb').MongoClient;
+const {MongoClient, ObjectID,}= require('mongodb');
 
 MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, client) => {
     if (err){
@@ -18,16 +19,16 @@ MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, client) => {
     //     console.log(JSON.stringify(result.ops, undefined, 2));
     // });
 
-    db.collection('Users').insertOne({
-        name: 'Lauren',
-        age: 28,
-        location: 'Atlanta'
-    }, (err, result)=> {
-        if (err){
-            return console.log('Unable to insert user', err);
-        }
-        console.log(JSON.stringify(result.ops, undefined, 2));
-    });
+    // db.collection('Users').insertOne({
+    //     name: 'Lauren',
+    //     age: 28,
+    //     location: 'Atlanta'
+    // }, (err, result)=> {
+    //     if (err){
+    //         return console.log('Unable to insert user', err);
+    //     }
+    //     console.log(result.ops[0]._id.getTimestamp());
+    // });
 
 
     client.close();
