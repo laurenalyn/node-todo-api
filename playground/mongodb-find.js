786e4056a22369fec09886a784e8b6118dc29b53
laurenalyn <laurenalynwilkerson@gmail.com>
2018-11-08 20:26:38 -0500

    retreives doc by _id

--- playground/mongodb-find.js
+++ playground/mongodb-find.js
@@ -16,9 +16,22 @@ MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, client) => {
     //     }, (err) => {
     //     console.log('Unable to fetch todos', err);
     // });
+
+
     // retreives all docs from todo collection that are not completed
-    db.collection('Todos').find({completed:false}).toArray()
-    .then( (docs) => {
+//     db.collection('Todos').find({completed:false}).toArray()
+//     .then( (docs) => {
+//         console.log('Todos');
+//         console.log(JSON.stringify(docs, undefined, 2));
+//     }, (err) => {
+//     console.log('Unable to fetch todos', err);
+// });
+
+
+// retreives doc using the _id
+db.collection('Todos').find({
+    _id:new ObjectID('5be4d651b4f808218d1fc14c')
+    }).toArray().then( (docs) => {
         console.log('Todos');
         console.log(JSON.stringify(docs, undefined, 2));
     }, (err) => {
