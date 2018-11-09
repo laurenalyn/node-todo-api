97c2b55abdece98323b3dc5967cf2b064db4a021
laurenalyn <laurenalynwilkerson@gmail.com>
2018-11-08 20:19:38 -0500

    query that retreives all documents from todos collection

--- playground/mongodb-find.js
+++ playground/mongodb-find.js
@@ -8,5 +8,15 @@ MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, client) => {
     console.log('Connected to MongoDB server');
     const db = client.db('TodoApp');
 
-    client.close();
+    // retreives all collectins from Todos collection converts them into an array and prints them to the screen
+    db.collection('Todos').find().toArray()
+        .then( (docs) => {
+            console.log('Todos');
+            console.log(JSON.stringify(docs, undefined, 2));
+        }, (err) => {
+        console.log('Unable to fetch todos', err);
+    });
+
+
+    // client.close();
 });
