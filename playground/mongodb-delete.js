243f9d519f272b743f9bce3c469b54df08b1c209
laurenalyn <laurenalynwilkerson@gmail.com>
2018-11-08 21:16:18 -0500

    delete one - deletes one todo even if there is more than one with same value

--- playground/mongodb-delete.js
+++ playground/mongodb-delete.js
@@ -10,16 +10,14 @@ MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, client) => {
     const db = client.db('TodoApp');
 
     // deleteMany
-    // db.collection('Todos').deleteMany({text: 'eat lunch'})
-    //     .then((result) => {
-    //         console.log(result);
-    //     });
-
-    // deleteOne
-    db.collection('Todos').deleteOne({text: 'eat lunch'})
-        .then((result) =>{
+    db.collection('Todos').deleteMany({text: 'eat lunch'})
+        .then((result) => {
             console.log(result);
         });
+
+    // deleteOne
+    
+
     // findOneAndDelete
 
 
