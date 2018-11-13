617000cfa5a2a1b98ee96adc38eaae7aef3ce326
laurenalyn <laurenalynwilkerson@gmail.com>
2018-11-12 21:03:01 -0500

    sets up model

--- server/server.js
+++ server/server.js
@@ -2,3 +2,15 @@ const mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;  
 mongoose.connect('mongodb://localhose:27017/TodoApp');
+
+let Todo = mongoose.model('Todo', {
+    text: {
+        type: String
+    },
+    completed: {
+        type: Boolean
+    },
+    completedAt: {
+        type: Number
+    }
+});
