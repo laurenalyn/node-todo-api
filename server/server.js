4fd085b385d398e2732a28b8cbf4a46957833531
laurenalyn <laurenalynwilkerson@gmail.com>
2018-11-12 21:21:38 -0500

    writes 2 Todos to the db

--- server/server.js
+++ server/server.js
@@ -1,8 +1,9 @@
 const mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;  
-mongoose.connect('mongodb://localhose:27017/TodoApp');
+mongoose.connect('mongodb://localhost:27017/TodoApp');
 
+// sets up model and type of content
 let Todo = mongoose.model('Todo', {
     text: {
         type: String
@@ -13,4 +14,35 @@ let Todo = mongoose.model('Todo', {
     completedAt: {
         type: Number
     }
+});
+
+// creates a new instance of a Todo
+
+// let newTodo = new Todo({
+//     text: 'Cook dinner'
+// });
+
+// let newTodo = new Todo({
+//     text: 'Do the dishes',
+//     completed: false,
+//     completedAt: 0
+// });
+
+// saves new Todo to db and returns a promise
+// newTodo.save().then((doc) => {
+//     console.log('Saved todo', doc);
+// }, (e) => {
+//     console.log('Unable to save todo');
+// });
+
+let otherTodo = new Todo({
+    text: 'Eat ice cream',
+    completed: true,
+    completedAt: 1234
+});
+
+otherTodo.save().then((doc) => {
+    console.log('Saved todo', doc);
+}, (e) => {
+    console.log('Unable to save todo');
 });
